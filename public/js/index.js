dcb5c697d26a17fa797234f7484364592572bdf4
RobetoGalicia <robertogalicia420@gmail.com>
2019-04-21 00:10:44 -0500

    configurando google

--- public/js/index.js
+++ public/js/index.js
@@ -16,10 +16,22 @@ if('serviceWorker' in navigator){
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('ID: ' + profile.getId()); 
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  console.log('Email: ' + profile.getEmail());
+
+  var id_token = googleUser.getAuthResponse().id_token;
+
+  //console.log(id_token);
+
+  var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/google');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
 }
 
 
@@ -30,4 +42,6 @@ function signOut() {
     console.log('User signed out.');
   });
 }
+
+
 
--- server.js
+++ server.js
@@ -10,9 +10,19 @@ const hbs = require('hbs');
 
 let server = http.createServer(app);
 
-
-
 const port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json());
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '133982771167-4pqvk30vklglgbimbpo3hvl3fp6nf122.apps.googleusercontent.com';
+
+const {OAuth2Client} = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.CLIENT_ID);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -30,9 +40,36 @@ app.get('/', (req, res) => {
     res.render('index');
 
 });
+
 app.get('/verbs', (req, res) => {
 
     res.render('verbs');
+
+});
+
+async function verify( token ) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    console.log(payload.name);
+    
+  }
+  
+
+app.post('/google', (req,res) => {
+
+    let token = req.body.idtoken;
+
+    verify( token );
+
+    res.json({
+        token
+    });
 
 });
 
