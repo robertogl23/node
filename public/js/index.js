e8d29d307b80754b218a86b2ecbd51bc468c3c58
RobetoGalicia <robertogalicia420@gmail.com>
2019-04-30 14:05:28 -0500

    arreglando diseño

--- public/js/index.js
+++ public/js/index.js
@@ -84,9 +84,11 @@ function onSignIn(googleUser) {
 
 
 function signOut() {
+ 
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
+    
 
   });
 }
--- public/js/jquery-u.js
+++ public/js/jquery-u.js
@@ -9,17 +9,25 @@ function renderUse( personas ){
 
     for( var i = 0; i < personas.length; i++){
 
-        html +=    '<div class="li">';
-        html +=       '<div class="im">';
-        html +=       '</div>';
-        html +=        '<div class="na">';
-        html +=            '<img style="width:50px; height:50px;" src="'+personas[i].img+'">';
-        html +=            '<a data-id="'+ personas[i].id+'">'+ personas[i].nombre +'</a>';
-        html +=        '</div>';
-        html +=    '</div>';
+        html +=    '<div class="co1">'
+        html +=         '<div class="li">';
+        html +=             '<div class="im">';
+        html +=                 '<img style="width:50px; height:50px;" src="'+personas[i].img+'">';
+        html +=             '</div>';
+        html +=             '<div class="na">';
+        html +=                 '<a data-id="'+ personas[i].id+'">'+ personas[i].nombre +'</a>';
+        html +=             '</div>';
+        html +=         '</div>';
+        html +=     '</div>';
 
     }
 
+
+
     use.html(html);
 
+}
+
+function renderVerbs(){
+    
 }
